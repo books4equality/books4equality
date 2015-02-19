5c5c1acf112bfb403d598fee007b259923227840
Guido García <guido@tid.es>
2015-02-19 20:45:46 +0100

    first release

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    logger = require('./services/logger'),
+    routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+var server = http.createServer(app);
+server.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, function() {
+    logger.info('b4e listening on', server.address());
+});
+
+process.on('SIGTERM', function onsigterm() {
+    server.close(function onclose() {
+        process.exit(0);
+    });
+});
---
+++ services/logger.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var logger = require('logops');
+    
+module.exports = logger;
