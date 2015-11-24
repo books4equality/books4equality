87da9f21548b3ca4accbd7cde935c27ab762a2cf
Benjamin Quinn <bquinn1992@gmail.com>
2015-11-24 15:00:15 -0500

    Added Contact Page

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,19 @@
 var express = require('express'),
     cors = require('cors'),
     books = require('../services/books'),
-    organizations = require('../services/organizations');
+    nodemailer = require('nodemailer'),
+    bodyParser = require('body-parser'),
+    organizations = require('../services/organizations'),
+    validator = require("email-validator"),
+    Console = console.Console;
 
-var router = express.Router();
+var router = express.Router(); 
 
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json({ limit: '10kb' }));
 router.use(cors()); // CORS support allows third-party sites to consume the API
+
 
 router.get('/', function(req, res, next) {
     return res.json({status: 1});
@@ -56,6 +64,80 @@ router.get('/organizations', function(req, res, next) {
 
         return res.json(organizations);
     })
+});
+
+router.post('/contact',function(req, res, next){
+
+    //Check if required fields are filled
+    if(!req.body.contact.cName || !req.body.contact.email || !req.body.contact.message) {
+        res.render('contact', {
+                title: 'Contact',
+                page: 'Contact',
+                type: 'empty',
+                description: 'Email not successfully sent.'
+            });
+        return;
+    }
+
+    //check if valid email
+    var email_check = validator.validate(req.body.contact.email);
+
+    if(email_check == false){
+        res.render('contact', {
+                title: 'Contact',
+                page: 'Contact',
+                type: 'error',
+                description: 'Email not successfully sent.'
+            });
+        return;
+    }
+
+    //Setup mailer
+    var mailOpts, smtpTrans;
+   
+    var EMAIL_USER = process.env.GMAIL_SMTP_USER,
+        EMAIL_PASS = process.env.GMAIL_SMTP_PASS;
+
+
+    //Setup nodemailer transport    
+    smtpTrans = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: EMAIL_USER,
+            pass: EMAIL_PASS
+        }
+    });
+
+    //Only for if we are using the custom transporter, ie. gmail etc not direct
+    
+    mailOpts = {
+        from: req.body.contact.cName + ' &lt;' + req.body.contact.email + '&gt;',
+        to: 'blquinn@uvm.edu',
+        subject: 'B4E Contact message',
+        text: 'Name: ' + req.body.contact.cName + '\n' +
+            'Email: ' + req.body.contact.email + '\n' +
+            'Message: ' +req.body.contact.message + '\n'
+    };  
+    
+
+    smtpTrans.sendMail(mailOpts, function(error, info){
+        //Email not sent
+        if(error){
+            res.render('contact', {
+                title: 'Contact',
+                page: 'Contact',
+                type: 'error',
+                description: 'Email not successfully sent.'
+            });
+        }else{
+            res.render('contact',{
+                title: 'contact',
+                page: 'contact',
+                type: 'success',
+                description: 'Email successfully sent'
+            });
+        }
+    });
 });
 
 module.exports = router;
