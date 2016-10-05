810ef5e69426c168b0f1d568b72d0a54c7d04f25
Benjamin Quinn <bquinn1992@gmail.com>
2016-10-04 23:25:23 -0400

    Removed the reserve button if users are not signed in.

--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,52 @@
-'use strict';
+'use strict'
 var express = require('express'),
   schools = require('../services/schools'),
   router = express.Router()
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.render('index',
     { 'page_name': 'index' }
-  );
-});
+  )
+})
 
-router.get('/contact', function (req, res, next) {
+router.get('/contact', function (req, res) {
   res.render('contact',
     { 'page_name': 'contact' }
-  );
-});
+  )
+})
 
-router.get('/library', function (req, res, next) {
+router.get('/library', function (req, res) {
   schools.getSchoolIDs((error, schools) => {
     if (error) return res.status(500).send()
     return res.render('library',
       {
         'page_name': 'library',
+        'schoolID': req.session.user ? req.session.user.schoolID : null,
         'schools': schools
       }
     )
   })
 })
 
-router.get('/mission', function (req, res, next) {
+router.get('/mission', function (req, res) {
   res.render('mission',
     { 'page_name': 'mission' }
-  );
-});
+  )
+})
 
-router.get('/schedule', function (req, res, next) {
+router.get('/schedule', function (req, res) {
   res.render('schedule',
     { 'page_name': 'schedule' }
-  );
-});
+  )
+})
 
-router.get('/about_us', function (req, res, next) {
+router.get('/about_us', function (req, res) {
   res.render('about_us',
     { 'page_name': 'about_us' }
-  );
-});
+  )
+})
 
-router.get('/users', function (req, res, next) {
+router.get('/users', function (req, res) {
   schools.getSchoolIDs((error, schools) => {
     if (error) return res.status(500).send()
     return res.render('userViews/login',
@@ -55,6 +56,6 @@ router.get('/users', function (req, res, next) {
       }
     )
   })
-});
+})
 
-module.exports = router;
+module.exports = router
