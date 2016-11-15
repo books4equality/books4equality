9b09c8b637f83aab42bd3e09d7c1143d62e64a87
Benjamin Quinn <bquinn1992@gmail.com>
2016-11-15 16:35:09 -0500

    beggining of leaderboard developement

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,8 +1,10 @@
-'use strict';
+'use strict'
 
-var crypto = require("crypto"),
+var crypto = require('crypto'),
 	express = require('express'),
 	passport = require('passport'),
+	D3Node = require('d3-node'),
+	d3 = require('d3'),
 	LocalStrategy = require('passport-local').Strategy,
 	bodyParser = require('body-parser'),
 	multer = require('multer'),
@@ -10,47 +12,47 @@ var crypto = require("crypto"),
 	ObjectID = require('mongodb').ObjectID,
 	validations = require('../middlewares/validations'),
 	organizations = require('../services/organizations'),
-	logger = require('../services/logger');
+	logger = require('../services/logger')
 
 passport.use(new LocalStrategy(
 	function(username, password, done) {
-		var passwordHash = crypto.createHash("sha256").update(password, "utf8").digest("base64");
+		var passwordHash = crypto.createHash('sha256').update(password, 'utf8').digest('base64')
 
 		var criteria = {
 			email: username,
 			password: passwordHash
-		};
+		}
 
 		organizations.findOne(criteria, function(err, organization) {
 			if(err) {
-				logger.info('user auth failed %j', err);
-				return done(null, false, err);
+				logger.info('user auth failed %j', err)
+				return done(null, false, err)
 			} else {
-				logger.info('user info %j', organization);
-				return done(null, organization);
+				logger.info('user info %j', organization)
+				return done(null, organization)
 			}
 		})
 	}
-));
+))
 
 passport.serializeUser(function serialize(user, done) {
-	logger.debug('serialize %j', user);
-	done(null, user);
-});
+	logger.debug('serialize %j', user)
+	done(null, user)
+})
 
 passport.deserializeUser(function deserialize(user, done) {
-	logger.debug('deserialize %j', user);
-	return done(null, user);
-});
+	logger.debug('deserialize %j', user)
+	return done(null, user)
+})
 
-var router = express.Router();
+var router = express.Router()
 
-router.use(passport.initialize());
-router.use(passport.session());
+router.use(passport.initialize())
+router.use(passport.session())
 
 router.use(bodyParser.urlencoded({
 	extended: true
-}));
+}))
 
 var fileUploadOptions = {
 	inMemory: true, // XXX might not scale well !
@@ -58,35 +60,38 @@ var fileUploadOptions = {
 		files: 1
 	},
 	putSingleFilesInArray: true // XXX will be the default value in future multer releases
-};
-router.use(multer(fileUploadOptions));
+}
+router.use(multer(fileUploadOptions))
 
 router.get('/organizations',
 	function(req, res, next) {
-		res.render('organizations/index', {
-			'page_name': 'organizations'
-		});
+		organizations.find({}, (err, orgs) => {
+			return res.render('organizations/index', {
+				'page_name': 'organizations',
+				'organizations': orgs
+			})
+		})
 	}
-);
+)
 
 router.get('/organizations/login',
 	function(req, res, next) {
-		res.render('organizations/login');
+		res.render('organizations/login')
 	}
-);
+)
 
 router.get('/organizations/randomstuff34lk12123euyv12ib1eb',
 	function(req, res, next) {
-		res.render('organizations/sign_up');
+		res.render('organizations/sign_up')
 	}
-);
+)
 
 router.get('/organizations/home',
 	login.ensureLoggedIn('/organizations/login'),
 	function(req, res, next) {
-		res.render('organizations/home');
+		res.render('organizations/home')
 	}
-);
+)
 
 router.post('/organizations/login',
 	login.ensureNotLoggedIn(),
@@ -94,40 +99,40 @@ router.post('/organizations/login',
 		successRedirect: '/organizations/home',
 		failureRedirect: '/organizations/login?unauthorized'
 	})
-);
+)
 
 router.get('/organizations/logout',
 	function(req, res) {
-		req.logout();
-		res.redirect('/organizations');
+		req.logout()
+		res.redirect('/organizations')
 	}
-);
+)
 
 router.get('/organizations/:id/logo',
 	// XXX could use Etag headers to cache images in the browser and save some juice
 	function(req, res) {
 		var criteria = {
 			_id: new ObjectID(req.params.id) // XXX routers shouldn't know about mongo details
-		};
+		}
 		organizations.findOne(criteria, function(err, organization) {
 			if(err) {
 				// TODO serve empty image
-				res.set('Content-Type', 'image/png');
-				return res.status(200);
+				res.set('Content-Type', 'image/png')
+				return res.status(200)
 			}
 
-			res.set('Content-Length', organization.logo.size);
-			res.set('Content-Type', organization.logo.mimetype);
-			res.write(organization.logo.data.buffer);
-			res.end();
-		});
+			res.set('Content-Length', organization.logo.size)
+			res.set('Content-Type', organization.logo.mimetype)
+			res.write(organization.logo.data.buffer)
+			res.end()
+		})
 	}
-);
+)
 
 router.post('/organizations/signup',
 	validations.validate('organization'),
 	function(req, res, next) {
-		var passwordHash = crypto.createHash("sha256").update(req.body.password, "utf8").digest("base64");
+		var passwordHash = crypto.createHash('sha256').update(req.body.password, 'utf8').digest('base64')
 		var organization = {
 			email: req.body.email,
 			password: passwordHash, // do not store plain password
@@ -136,23 +141,23 @@ router.post('/organizations/signup',
 			numberBooks: 0,
 			outreach: 0,
 			createdAt: new Date()
-		};
+		}
 
 		if(req.files.logo) {
 			organization.logo = {
 				mimetype: req.files.logo[0].mimetype,
 				size: req.files.logo[0].size,
 				data: req.files.logo[0].buffer // XXX works with inMemory multer, otherwise read from logo.path
-			};
+			}
 		}
 		organizations.insert(organization, function(err, result) {
 			if(err) {
-				return next(new Error('Not able to create organization'));
+				return next(new Error('Not able to create organization'))
 			}
 
-			return res.redirect('/organizations/login');
-		});
+			return res.redirect('/organizations/login')
+		})
 	}
-);
+)
 
-module.exports = router;
+module.exports = router
